e5d997b8c217cd8737b3f7f7bc7cd0185411e3be
Joel Kasisi <joelkasisi10@gmail.com>
2024-10-21 15:47:28 +0200

    server fix

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const weatherRoutes_1 = __importDefault(require("./routes/weatherRoutes"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: 'http://localhost:5173' }));
+app.use((0, cors_1.default)({ origin: ['http://localhost:5173', 'https://nga-weather.onrender.com'], credentials: true }));
 app.use(express_1.default.json());
 app.use('/api/weather', weatherRoutes_1.default);
 exports.default = app;
